3fa71692da03f00028b5db84325c5deaad154668
francescoganci <76791249+francescoganci@users.noreply.github.com>
2020-12-30 19:55:40 +0000

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,7 @@
+def Triplo(num: number):
+    basic.show_string("3")
+    basic.show_string("x")
+    basic.show_string("" + str((num)))
+    basic.show_string("=")
+    basic.show_number(3 * num)
+Triplo(2)
---
+++ main.ts
@@ -0,0 +1,8 @@
+function Triplo (num: number) {
+    basic.showString("3")
+    basic.showString("x")
+    basic.showString("" + (num))
+    basic.showString("=")
+    basic.showNumber(3 * num)
+}
+Triplo(2)
